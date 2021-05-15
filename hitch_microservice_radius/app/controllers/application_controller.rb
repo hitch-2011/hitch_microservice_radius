756fc67d7096ee7a5f5cc6ffa38c61141c06f552
Dominic Padula <domopadula2192@gmail.com>
2021-05-15 11:06:44 -0600

    create zipcode and radius for routes

---
+++ hitch_microservice_radius/app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/base'
+require 'dotenv'
+require 'dotenv/load'
+require 'pry'
+require 'faraday'
+require 'rack/test'
+require 'json'
+require 'fast_jsonapi'
+require './models/radius'
+require './app/services/radius_service'
+
+class ApplicationController < Sinatra::Base
+before do
+  content_type :json
+end
+
+  get '/:zipcode/:radius' do
+  radius = ZipCodeRadiusService.find_radius(params)
+  data = Radius.new(radius).to_json
+  end
+end
