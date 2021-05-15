9578e03dacfbb8ca0a892624846433ac77a01f4b
Dominic Padula <domopadula2192@gmail.com>
2021-05-15 11:09:25 -0600

    create service for endpoint
    Co-authored-by: Jake Volpe <62768837+javolpe@users.noreply.github.com>
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

---
+++ hitch_microservice_radius/app/services/radius_service.rb
@@ -0,0 +1,22 @@
+class ZipCodeRadiusService
+  class << self
+
+    def find_radius(params)
+      response = conn.get('/api/v1/radius') do |req|
+        req.params['code'] = params[:zipcode]
+        req.params['radius'] = params[:radius]
+        req.params['unit'] = 'miles'
+        req.params['country'] = 'us'
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    private
+
+  def conn
+     @conn ||= Faraday.new('https://app.zipcodebase.com') do |req|
+       req.params['apikey'] = ENV['ZIPCODEBASE_APIKEY']
+      end
+    end
+  end
+end
